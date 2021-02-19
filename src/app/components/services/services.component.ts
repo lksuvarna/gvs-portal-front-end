f252954ea5d7406a21bf56eea409a8e126e7bc84
vratnesh <vratnesh@in.ibm.com>
2021-02-19 09:43:49 +0530

    Country Middle frame

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -6,51 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-  cloudantData: any = []
   servicesData: any = []
 
   constructor() { }
 
   ngOnInit(): void {
-     
-  this.cloudantData  = {
-    "code": "744",
-    "name": "India",
-    "isocode": "IN",
-    "isjabber": true,
-    "isfixedphone": false,
-    "isfac": true,
-    "isspecial": false
-  }
-
-  const servicesData = { 
-  "data": [
-    {    
-      "lhs": [
-        {"name" : "Services", "indented" : false, "highlighted": true},
-        {"name" : "Approvals Pending", "indented" : false, "highlighted": false},
-        {"name" : "Revalidation Pending", "indented" : false, "highlighted": false},
-        {"name" : "Resources", "indented" : false, "highlighted": false},
-        {"name" : "Requests", "indented" : false, "highlighted": false}
-      ],
-      "services" : ["Jabber", "Fixed Phone", "FAC Code","Special Request"], 
-      "titles": [
-        "Terms of use",
-        "Useful Information",
-        "Please bear in mind the following points when making a request :"
-      ],
-      "usefulinfotexts": [
-        "To make a request the Employee must exist in BluePages (except for cancellation requests).",
-        "You must know the IBM serial Number of the person making the request.",
-        "Only one request per employee per request type is processed at a time."
-      ],
-      "termsurl": "https://w3.ibm.com/w3/info_terms_of_use.html"
+    const servicesData = { 
+      "data": [
+        {    
+          "lhs": [
+            {"name" : "Services", "indented" : false, "highlighted": true},
+            {"name" : "Approvals Pending", "indented" : false, "highlighted": false},
+            {"name" : "Revalidation Pending", "indented" : false, "highlighted": false},
+            {"name" : "Resources", "indented" : false, "highlighted": false},
+            {"name" : "Requests", "indented" : false, "highlighted": false}
+          ]
+        }
+      ]
     }
-  ]
-}
-  
-  this.servicesData = servicesData.data[0]
-
+      
+      this.servicesData = servicesData.data[0]
+    
   }
 }
 
