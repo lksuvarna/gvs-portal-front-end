f6171d53226d810843f719ed7eec58d9764cc21f
ashim159 <ashim159@in.ibm.com>
2021-03-04 16:18:00 +0530

    Updated review details apge and entry details page

--- src/app/components/voip-in-new/voip-in-new.component.ts
+++ src/app/components/voip-in-new/voip-in-new.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { cloudantservice } from '../../_services/cloudant.service';
 import { CookieHandlerService } from '../../_services/cookie-handler.service';
 import { NgForm } from '@angular/forms';
-import {Router} from  '@angular/router';
+import { Router} from  '@angular/router';
 
 @Component({
   selector: 'app-voip-in-new',
@@ -11,54 +11,88 @@ import {Router} from  '@angular/router';
 })
 export class VoipInNewComponent implements OnInit {
 
-  Location = [{'Campus':'Select Office Location','Buildings':[]},
-    {'Campus':'Banglore','Buildings':['Select One','MTP','SA']},
-  {'Campus':'Gurgaon','Buildings':['Select One','DLF Infinity','ASF']}
-];
-campus:string[] | undefined;
-  array = [];
-  SelectedLoc: Array<string> = [];
-  Banglore = ['Select One','MTP','SA'];
-  Gurgaon = ['Select One','DLF Infinity','ASF']; 
+  Locations:any = {
+    locc : ['Select Office Location~~Select One','Banglore~~MTP','Banglore~~SA',
+    'Gurgaon~~DLF Infinity','Gurgaon~~ASF','Hyderabad~~Hitech']
+    };
+    campA: any = [];
+    camp: any = [];
+    buildA: any = [];
+    build: any = [];
+    j = 0;
+
+    // Dummy:any = ['any','any1','any2'];
+
+  isEntryForm = false;
+  isReviewForm = true;
+  Voice_Type = "No";
+  reviewDetailsIndia = {
+
+    officeLocation:	"Ahmedabad",
+    campus:	"Titanium",
+    funded:	"Yes",
+    chargeDepartmentCode:	"QADF",
+    businessUnit:	"GBS",
+    projectId: "ABCDS",
+    fixPhoneIdentifier: " "
+
+  }
+  
+  // Location = [{'Campus':'Select Office Location','Buildings':[]},
+  //   {'Campus':'Banglore','Buildings':['Select One','MTP','SA']},
+  //   {'Campus':'Gurgaon','Buildings':['Select One','DLF Infinity','ASF']}
+  // ];
+
+  // campus:string[] | undefined;
+  //   array = [];
+  //   SelectedLoc: Array<string> = [];
+  //   Banglore = ['Select One','MTP','SA'];
+  //   Gurgaon = ['Select One','DLF Infinity','ASF']; 
+    
   hideDeptCode = true;
   hideBuilding = true;
-  countryname:any;
-  ccode='';
-  cloudantData: any = []
-  servicesData: any = []
+    countryname:any;
+    ccode='';
+    cloudantData: any = []
+    servicesData: any = []
+
   constructor(private router:Router,private cookie: CookieHandlerService,private cloudantservice:cloudantservice) { 
-   //alert(this.Location[0].Buildings[2]);
-   //alert(this.Location[0].Campus);
- 
+    
+    for(var i=0;i<this.Locations.locc.length;i++) {
+      var n=this.Locations.locc[i].indexOf("~")
+      this.campA[i] = this.Locations.locc[i].substr(0,n);
+      this.buildA[i] = this.Locations.locc[i].substr(n+2,this.Locations.locc[i].length);
+      }
+      for (var i=0;i<this.campA.length;i++) {
+      if(this.campA[i] !=this.campA[i+1]) {
+      this.camp[this.j] = this.campA[i];
+      this.j++;
+      }
+      }
   
   }
-  submit(){
-    this.router.navigate(['/reviewdetails']) 
-  }
+  // submit(){
+  //   this.router.navigate(['/reviewdetails']) 
+  // }
   selectedLocation(loc:String) {
+    
     if(loc.toUpperCase() != 'SELECT OFFICE LOCATION') {
       this.hideBuilding = false;
-      
+      var k =0; 
 
-    /*  if(loc == this.Campus[0].Banglore) {
-        this.array = Object.values(this.Campus[0]);
+      this.build[k] = this.buildA[0];
+      for(var i=0;i<this.campA.length;i++) {
+      if(loc == this.campA[i]) {
+      this.build[++k] = this.buildA[i];
       }
-      else if(this.SelectedLoc == Object.keys(this.Campus[1])) {
-        this.SelectedLoc = [...this.Gurgaon];
-      }
-      else {
-        return;
-      } */
-      for(var i=0;i<this.Location.length;i++) {
-        if(loc == this.Location[i].Campus) {
-          this.campus = [...this.Location[i].Buildings];
-        }
-      }
-     
+      }  
+    // alert("HIIII"+this.build);
+           
     } else {
       this.hideBuilding = true;
     }
   }
+
   showChargeDepartmentCode() {
     this.hideDeptCode = false;
   }
@@ -68,6 +102,7 @@ campus:string[] | undefined;
   }
 
   entryDetails(formData: NgForm) {
+    
     if(formData.value.Location_1.toUpperCase() == 'SELECT OFFICE LOCATION' || formData.value.Location_1 == '') {
       alert('Please select the Office Location');
       return;
@@ -83,8 +118,25 @@ campus:string[] | undefined;
     if(formData.value.Projectid == '') {
       alert('Please enter the Project Id');
       return;
-    }
+    }    
+
+    this.isEntryForm = true;
+    this.isReviewForm = false;
+
+    this.reviewDetailsIndia.officeLocation = formData.value.Location_1;
+    this.reviewDetailsIndia.campus = formData.value.Buildings;
+    this.reviewDetailsIndia.funded = this.Voice_Type;
+    this.reviewDetailsIndia.chargeDepartmentCode = formData.value.Department_number;
+    // this.reviewDetailsIndia.businessUnit = formData.value
+    this.reviewDetailsIndia.projectId = formData.value.Projectid;
+    this.reviewDetailsIndia.fixPhoneIdentifier = formData.value.identifier_hp;
   }
+
+  BackButton() {
+    this.isEntryForm = false;
+    this.isReviewForm = true;
+  }
+
 
   ngOnInit(): void {
      
@@ -102,35 +154,36 @@ campus:string[] | undefined;
       "isfac": this.countryname.isfac,
       "isspecial": this.countryname.isspecial
     }
-  });
+    });
     const servicesData = { 
-    "data": [
-      {    
-        "lhs": [
-          {"name" : "Services","routingname":"/services", "indented" : false, "highlighted": true},            
-          {"name" : "Approvals Pending","routingname":"/inprogress", "indented" : false, "highlighted": false},
-          {"name" : "Revalidation Pending","routingname":"/inprogress", "indented" : false, "highlighted": false},
-          {"name" : "Resources","routingname":"/inprogress", "indented" : false, "highlighted": false},
-          {"name" : "Requests","routingname":"/requests", "indented" : false, "highlighted": false}
-        ],
-        "services" : ["Jabber", "Fixed Phone", "FAC Code","Special Request"], 
-        "titles": [
-          "Terms of use",
-          "Useful Information",
-          "Please bear in mind the following points when making a request :"
-        ],
-        "usefulinfotexts": [
-          "To make a request the Employee must exist in BluePages (except for cancellation requests).",
-          "You must know the IBM serial Number of the person making the request.",
-          "Only one request per employee per request type is processed at a time."
-        ],
-        "termsurl": "https://w3.ibm.com/w3/info_terms_of_use.html"
-      }
-    ]
+      "data": [
+        {    
+          "lhs": [
+            {"name" : "Services","routingname":"/services", "indented" : false, "highlighted": true},            
+            {"name" : "Approvals Pending","routingname":"/inprogress", "indented" : false, "highlighted": false},
+            {"name" : "Revalidation Pending","routingname":"/inprogress", "indented" : false, "highlighted": false},
+            {"name" : "Resources","routingname":"/inprogress", "indented" : false, "highlighted": false},
+            {"name" : "Requests","routingname":"/requests", "indented" : false, "highlighted": false}
+          ],
+          "services" : ["Jabber", "Fixed Phone", "FAC Code","Special Request"], 
+          "titles": [
+            "Terms of use",
+            "Useful Information",
+            "Please bear in mind the following points when making a request :"
+          ],
+          "usefulinfotexts": [
+            "To make a request the Employee must exist in BluePages (except for cancellation requests).",
+            "You must know the IBM serial Number of the person making the request.",
+            "Only one request per employee per request type is processed at a time."
+          ],
+          "termsurl": "https://w3.ibm.com/w3/info_terms_of_use.html"
+        }
+      ]
+    }
+      
+      this.servicesData = servicesData.data[0]
+  
   }
     
-    this.servicesData = servicesData.data[0]
-  
-    }
-  }
+}
   
