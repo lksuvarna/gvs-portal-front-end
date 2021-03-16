bc6ad64756bc129ffb4be50c846a89416aee7490
vratnesh <vratnesh@in.ibm.com>
2021-03-16 19:23:34 +0530

    Top Navigation Highlight

--- src/app/components/uitoplinks/uitoplinks.component.ts
+++ src/app/components/uitoplinks/uitoplinks.component.ts
@@ -11,6 +11,7 @@ import {Router} from  '@angular/router';
 export class UitoplinksComponent implements OnInit {
   searchText = '';
   ccode='';
+  routerPath = ''
   searchItems = [    
     {"name" : "India","code":"744","path":"././assets/flags/744.png"},
     {"name" : "France","code":"706","path":"././assets/flags/706.png"},
@@ -22,8 +23,19 @@ export class UitoplinksComponent implements OnInit {
 
   ngOnInit(): void {
     this.ccode=this.cookie.getCookie('ccode').substring(6,9);
+
+    this.routerPath = window.location.pathname
+
+    if (this.routerPath === '/'){
+      this.toggleHighlight('home-nav')
+    } else if (this.routerPath === '/feedback') {
+      this.toggleHighlight('feed-nav')
+    } else {
+      this.toggleHighlight('services-nav')
+    }
     
   }
+  
 
   onClick(event:Event) {
     if (!this._eref.nativeElement.contains(event.target))
@@ -36,9 +48,32 @@ export class UitoplinksComponent implements OnInit {
     this.searchText = ''
   }
 
-  closeDropdown() {
+  closeDropdown(id: any) {
     document.getElementById('countrydropdown')?.classList.remove('show');
     this.searchText = ''
+    this.toggleHighlight(id)
+    }
+
+  toggleHighlight(id : any){
+          
+    if (id ==='home-nav'){
+      document.getElementById('home-nav')?.classList.add('ds-bg-neutral-5')
+      document.getElementById('services-nav')?.classList.remove('ds-bg-neutral-5')
+      document.getElementById('feed-nav')?.classList.remove('ds-bg-neutral-5')
+    } 
+
+    if (id ==='services-nav'){
+      document.getElementById('home-nav')?.classList.remove('ds-bg-neutral-5')
+      document.getElementById('services-nav')?.classList.add('ds-bg-neutral-5')
+      document.getElementById('feed-nav')?.classList.remove('ds-bg-neutral-5')
+    } 
+
+    if (id ==='feed-nav'){
+      document.getElementById('home-nav')?.classList.remove('ds-bg-neutral-5')
+      document.getElementById('services-nav')?.classList.remove('ds-bg-neutral-5')
+      document.getElementById('feed-nav')?.classList.add('ds-bg-neutral-5')
+    } 
+
   }
 
   
