b6a2ac256981e3d14143e84cea7b1b709e7b5049
HARSHITHA ARAVA <hararava@in.ibm.com>
2021-04-16 20:28:36 +0530

    language translation

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -70,6 +70,7 @@ const appRoutes: Routes = [
     { path: 'pagenotfound', component: PagenotfoundComponent},
     { path: 'pagenotfound?error', component: PagenotfoundComponent},
     { path: 'entrydetailsfacin', component: FacInNewComponent},
+    
    // {path: '**', redirectTo:'/pagenotfound?error'}
    {path: '**', component: PagenotfoundComponent}
     
