f0c60e5589445016a05068510fc6d462a4879d1c
Swarnava Choudhury <swarnava_choudhury@in.ibm.com>
2021-04-23 12:39:44 +0530

    fix up

--- src/app/components/fac-in-update/fac-in-update.component.ts
+++ src/app/components/fac-in-update/fac-in-update.component.ts
@@ -193,6 +193,7 @@ export class FacInUpdateComponent implements OnInit {
     this.authLevel = formData.value.authLevel
     this.bj_disp=formData.value.businessjustification.replace(/[\n\r+]/g, ' ');
     this.isReviewForm = false;
+    
     this.isEntryForm = true;
   }
   // Submit to Snow Jabber new code added by Swarnava ends	
