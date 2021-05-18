e771059f157e33df41cdf95cf187eae69ef6a1cd
Swarnava Choudhury <swarnava_choudhury@in.ibm.com>
2021-05-18 10:39:10 +0530

    Harshtha code update

--- dist/gvs-portal-front-end/main.js
+++ dist/gvs-portal-front-end/main.js
@@ -941,7 +941,7 @@ function gettime() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\HARSHITHAARAVA\Desktop\Cirrus Latest\Cirrus latest new\gvs-portal-front-end\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Swarnavo\GVS_Portal\gvs-portal-front-end\src\main.ts */"zUnb");
 
 
 /***/ }),
@@ -24833,20 +24833,18 @@ class ApprovalpendingComponent {
                 this.snowaction = 'snow_approve';
                 sessionStorage.setItem('reval', 'approval');
             }
-            this.empserial = "467756744";
+            //this.empserial="467756744";
             if (this.pcode == this.ccode) {
-                setTimeout(() => {
-                    this.servicenowservice.searchsnowcoments(this.empserial, this.snowaction, "", "").subscribe(data => {
-                        console.log(' snow response', data.message);
-                        console.log(' snow response', data.message.length);
-                        if (data.message.length == 0)
-                            this.errorinfo = false;
-                        else {
-                            this.pendingRequest_original = data.message;
-                            this.pendingRequest = this.pendingRequest_original;
-                        }
-                    });
-                }, 400);
+                this.servicenowservice.searchsnowcoments(this.empserial, this.snowaction, "", "").subscribe(data => {
+                    console.log(' snow response', data.message);
+                    console.log(' snow response', data.message.length);
+                    if (data.message.length == 0)
+                        this.errorinfo = false;
+                    else {
+                        this.pendingRequest_original = data.message;
+                        this.pendingRequest = this.pendingRequest_original;
+                    }
+                });
             }
             else {
                 this.errorinfo = false;
