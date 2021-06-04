5fca7ecea19d5a73aa6f1fc99ee8a34aa105b8e1
Swarnava Choudhury <swarnava_choudhury@in.ibm.com>
2021-06-04 21:38:14 +0530

    Msg Fix Harshitha

--- dist/gvs-portal-front-end/main.js
+++ dist/gvs-portal-front-end/main.js
@@ -1149,7 +1149,7 @@ function gettime() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\HARSHITHAARAVA\Desktop\Cirrus Latest\Cirrus latest new\gvs-portal-front-end\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Swarnavo\GVS_Portal\gvs-portal-front-end\src\main.ts */"zUnb");
 
 
 /***/ }),
