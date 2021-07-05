895545bf9c01672f1e90aaae6ca3666329787447
Swarnava Choudhury <swarnava_choudhury@in.ibm.com>
2021-07-05 13:44:17 +0530

    LA update label VM change + icon change

--- dist/gvs-portal-front-end/main.js
+++ dist/gvs-portal-front-end/main.js
@@ -1153,7 +1153,7 @@ function gettime() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\HARSHITHAARAVA\Desktop\Cirrus Latest\Cirrus latest new\gvs-portal-front-end\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Swarnavo\GVS_Portal\gvs-portal-front-end\src\main.ts */"zUnb");
 
 
 /***/ }),
