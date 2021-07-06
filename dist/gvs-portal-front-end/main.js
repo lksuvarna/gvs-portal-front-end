b49f27440765e436540fb78dcfd8748043a6e37c
pavanb15 <pavanb15@in.ibm.com>
2021-07-06 18:49:06 +0530

    Indonesia Fixed Phone update defect fix

--- dist/gvs-portal-front-end/main.js
+++ dist/gvs-portal-front-end/main.js
@@ -31893,7 +31893,7 @@ function HpIndonesiaUpdateComponent_tr_36_Template(rf, ctx) { if (rf & 1) {
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](4, "br");
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](5, "div", 49);
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](6, "p");
-    _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](7, "No record was found for the Mac Address or Phone Number. Please verify and try again.");
+    _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](7, "No record was found for the MAC Address or Phone Number. Please verify and try again.");
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
     _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
